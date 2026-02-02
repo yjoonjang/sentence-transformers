5c222a8a4f63851dd7239b55222fbb5b21c09c38
Omkar Kabde <omkarkabde@gmail.com>
2026-02-02 18:54:24 +0530

    migrate `train_stsb_ct_improved` from v2 to v3 (#3627)
    * migrate `train_stsb_ct_improved` from v2 to v3
    
    * simplify logic, add steps and model save
    
    * simplify evaluators
    
    * Update model push to hub message for STSB CT
    
    * Final nits, looks good now
    
    ---------
    
    Co-authored-by: Tom Aarsen <Cubiegamedev@gmail.com>

--- examples/sentence_transformer/unsupervised_learning/CT_In-Batch_Negatives/train_stsb_ct-improved.py
+++ examples/sentence_transformer/unsupervised_learning/CT_In-Batch_Negatives/train_stsb_ct-improved.py
@@ -2,18 +2,22 @@ import logging
 from datetime import datetime
 
 from datasets import load_dataset
-from torch.utils.data import DataLoader
 
-from sentence_transformers import InputExample, LoggingHandler, SentenceTransformer, losses, models, util
+from sentence_transformers import LoggingHandler, SentenceTransformer, losses, models, util
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
 
-#### Just some code to print debug information to stdout
+# Just some code to print debug information to stdout
 logging.basicConfig(
-    format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO, handlers=[LoggingHandler()]
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    handlers=[LoggingHandler()],
 )
-#### /print debug information to stdout
+# print debug information to stdout
 
-## Training parameters
+# Training parameters
 model_name = "distilbert-base-uncased"
 batch_size = 128
 epochs = 1
@@ -24,58 +28,78 @@ model_save_path = "output/training_stsb_ct-improved-{}-{}".format(
     model_name, datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 )
 
-
-################# Train sentences #################
+# Train sentences
 # We use 1 Million sentences from Wikipedia to train our model
-wikipedia_dataset = load_dataset("sentence-transformers/wiki1m-for-simcse", split="train")
+train_dataset = load_dataset("sentence-transformers/wiki1m-for-simcse", split="train[:1000]")
+train_dataset = train_dataset.map(lambda x: {"text": x["text"].strip()})
+print(train_dataset)
 
+# Download and load STSb
+dev_sts_dataset = load_dataset("sentence-transformers/stsb", split="validation")
+test_sts_dataset = load_dataset("sentence-transformers/stsb", split="test")
 
-# train_sentences are simply your list of sentences
-train_sentences = [
-    InputExample(texts=[example["text"].strip(), example["text"].strip()]) for example in wikipedia_dataset
-]
+dev_evaluator = EmbeddingSimilarityEvaluator(
+    dev_sts_dataset["sentence1"],
+    dev_sts_dataset["sentence2"],
+    dev_sts_dataset["score"],
+    name="sts-dev",
+)
 
-################## Download and load STSb #################
-sts_dataset = load_dataset("sentence-transformers/stsb")
+test_evaluator = EmbeddingSimilarityEvaluator(
+    test_sts_dataset["sentence1"],
+    test_sts_dataset["sentence2"],
+    test_sts_dataset["score"],
+    name="sts-test",
+)
 
-dev_samples = []
-test_samples = []
-
-for row in sts_dataset["validation"]:
-    dev_samples.append(InputExample(texts=[row["sentence1"], row["sentence2"]], label=row["score"]))
-
-for row in sts_dataset["test"]:
-    test_samples.append(InputExample(texts=[row["sentence1"], row["sentence2"]], label=row["score"]))
-
-dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name="sts-dev")
-test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name="sts-test")
-
-################# Initialize an SBERT model #################
+# Initialize an SBERT model
 word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-
-# For ContrastiveTension we need a special data loader to construct batches with the desired properties
-train_dataloader = DataLoader(train_sentences, batch_size=batch_size, shuffle=True, drop_last=True)
-
-# As loss, we losses.ContrastiveTensionLoss
+# Loss
 train_loss = losses.ContrastiveTensionLossInBatchNegatives(model, scale=1, similarity_fct=util.dot_score)
 
-
-# Train the model
-model.fit(
-    train_objectives=[(train_dataloader, train_loss)],
-    evaluator=dev_evaluator,
-    epochs=1,
-    evaluation_steps=1000,
-    warmup_steps=1000,
-    output_path=model_save_path,
-    optimizer_params={"lr": 5e-5},
-    use_amp=True,  # Set to True, if your GPU supports FP16 cores
+# Prepare the training arguments
+args = SentenceTransformerTrainingArguments(
+    output_dir=model_save_path,
+    num_train_epochs=epochs,
+    per_device_train_batch_size=batch_size,
+    warmup_ratio=0.1,
+    eval_strategy="steps",
+    eval_steps=0.1,
+    logging_steps=0.01,
+    learning_rate=5e-5,
+    save_strategy="no",
+    fp16=True,
 )
 
-########### Load the model and evaluate on test set
+# Trainer
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    evaluator=dev_evaluator,
+    loss=train_loss,
+)
 
-model = SentenceTransformer(model_save_path)
+# Train the model
+trainer.train()
+
+dev_evaluator(model)
 test_evaluator(model)
+
+final_output_path = model_save_path + "-final"
+model.save_pretrained(final_output_path)
+
+# (Optional) save the model to the Hugging Face Hub!
+# It is recommended to run `huggingface-cli login` to log into your Hugging Face account first
+model_name = model_name if "/" not in model_name else model_name.split("/")[-1]
+try:
+    model.push_to_hub(f"{model_name}-stsb-ct")
+except Exception:
+    logging.error(
+        f"Error uploading model to the Hugging Face Hub:\nTo upload it manually, you can run "
+        f"`huggingface-cli login`, followed by loading the model using `model = SentenceTransformer({final_output_path!r})` "
+        f"and saving it using `model.push_to_hub('{model_name}-stsb-ct')`."
+    )
