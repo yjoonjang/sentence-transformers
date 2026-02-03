ff866c82ee858b450eedc263a7609a1dd933bef1
Omkar Kabde <omkarkabde@gmail.com>
2026-02-03 13:38:30 +0530

    migrate `train_stsb_simcse.py` from v2 to v3 (#3648)
    * migrate `train_stsb_simcse.py` from v2 to v3
    
    * Tiny updates, looks great
    
    ---------
    
    Co-authored-by: Tom Aarsen <Cubiegamedev@gmail.com>

--- examples/sentence_transformer/unsupervised_learning/SimCSE/train_stsb_simcse.py
+++ examples/sentence_transformer/unsupervised_learning/SimCSE/train_stsb_simcse.py
@@ -1,12 +1,14 @@
 import logging
-import math
 from datetime import datetime
 
 from datasets import load_dataset
-from torch.utils.data import DataLoader
 
-from sentence_transformers import InputExample, LoggingHandler, SentenceTransformer, losses, models
+from sentence_transformers import LoggingHandler, SentenceTransformer
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers.losses import MultipleNegativesRankingLoss
+from sentence_transformers.models import Pooling, Transformer
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
 
 # Just some code to print debug information to stdout
 logging.basicConfig(
@@ -26,70 +28,98 @@ model_save_path = "output/training_stsb_simcse-{}-{}-{}".format(
 )
 
 # Here we define our SentenceTransformer model
-word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
-pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+word_embedding_model = Transformer(model_name, max_seq_length=max_seq_length)
+pooling_model = Pooling(word_embedding_model.get_word_embedding_dimension())
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
 # We use 1 Million sentences from Wikipedia to train our model
-wikipedia_dataset = load_dataset("sentence-transformers/wiki1m-for-simcse", split="train")
+train_dataset = load_dataset("sentence-transformers/wiki1m-for-simcse", split="train")
 
 
-# train_samples is a list of InputExample objects where we pass the same sentence twice to texts, i.e. texts=[sent, sent]
-train_samples = []
-for example in wikipedia_dataset:
-    line = example["text"].strip()
-    if len(line) >= 10:
-        train_samples.append(InputExample(texts=[line, line]))
+# SimCSE (or InfoNCE) require positive pairs (or larger), so if we only have single sentences,
+# we can simply duplicate the sentences to create a weak dataset of positive pairs. The loss
+# will take negative samples from the other samples in the batch.
+def simcse_map(example):
+    text = example["text"].strip()
+    return {
+        "sentence1": text,
+        "sentence2": text,
+    }
 
-# Read STSbenchmark dataset and use it as development set
-logging.info("Read STSbenchmark dev dataset")
-sts_dataset = load_dataset("sentence-transformers/stsb")
 
-dev_samples = []
-test_samples = []
-
-for row in sts_dataset["validation"]:
-    dev_samples.append(InputExample(texts=[row["sentence1"], row["sentence2"]], label=row["score"]))
-
-for row in sts_dataset["test"]:
-    test_samples.append(InputExample(texts=[row["sentence1"], row["sentence2"]], label=row["score"]))
-
-dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
-    dev_samples, batch_size=train_batch_size, name="sts-dev"
+train_dataset = (
+    load_dataset("sentence-transformers/wiki1m-for-simcse", split="train")
+    .filter(lambda x: len(x["text"].strip()) >= 10)
+    .map(simcse_map, remove_columns=["text"])
 )
-test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
-    test_samples, batch_size=train_batch_size, name="sts-test"
+logging.info(train_dataset)
+
+# Download and load STSb
+eval_sts_dataset = load_dataset("sentence-transformers/stsb", split="validation")
+test_sts_dataset = load_dataset("sentence-transformers/stsb", split="test")
+
+dev_evaluator = EmbeddingSimilarityEvaluator(
+    sentences1=eval_sts_dataset["sentence1"],
+    sentences2=eval_sts_dataset["sentence2"],
+    scores=eval_sts_dataset["score"],
+    name="sts-dev",
+)
+test_evaluator = EmbeddingSimilarityEvaluator(
+    sentences1=test_sts_dataset["sentence1"],
+    sentences2=test_sts_dataset["sentence2"],
+    scores=test_sts_dataset["score"],
+    name="sts-test",
 )
 
 # We train our model using the MultipleNegativesRankingLoss
-train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size, drop_last=True)
-train_loss = losses.MultipleNegativesRankingLoss(model)
+train_loss = MultipleNegativesRankingLoss(model)
 
-warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1)  # 10% of train data for warm-up
-evaluation_steps = int(len(train_dataloader) * 0.1)  # Evaluate every 10% of the data
-logging.info(f"Training sentences: {len(train_samples)}")
-logging.info(f"Warmup-steps: {warmup_steps}")
 logging.info("Performance before training")
 dev_evaluator(model)
+test_evaluator(model)
 
-# Train the model
-model.fit(
-    train_objectives=[(train_dataloader, train_loss)],
-    evaluator=dev_evaluator,
-    epochs=num_epochs,
-    evaluation_steps=evaluation_steps,
-    warmup_steps=warmup_steps,
-    output_path=model_save_path,
-    optimizer_params={"lr": 5e-5},
-    use_amp=True,  # Set to True, if your GPU supports FP16 cores
+# Prepare training arguments
+args = SentenceTransformerTrainingArguments(
+    output_dir=model_save_path,
+    num_train_epochs=num_epochs,
+    per_device_train_batch_size=train_batch_size,
+    warmup_ratio=0.1,
+    eval_strategy="steps",
+    eval_steps=0.1,
+    logging_steps=0.01,
+    learning_rate=5e-5,
+    save_strategy="no",
+    fp16=True,  # If your GPU does not have FP16 cores, set fp16=False
 )
 
-##############################################################################
-#
-# Load the stored model and evaluate its performance on STS benchmark dataset
-#
-##############################################################################
+# Train the model
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    evaluator=dev_evaluator,
+    loss=train_loss,
+)
 
+logging.info("Start training")
+trainer.train()
 
-model = SentenceTransformer(model_save_path)
-test_evaluator(model, output_path=model_save_path)
+# Run evaluation
+logging.info("Performance after training")
+dev_evaluator(model)
+test_evaluator(model)
+
+latest_output_path = model_save_path + "-latest"
+model.save_pretrained(latest_output_path)
+
+# (Optional) save the model to the Hugging Face Hub!
+# It is recommended to run `huggingface-cli login` to log into your Hugging Face account first
+model_name = model_name if "/" not in model_name else model_name.split("/")[-1]
+try:
+    model.push_to_hub(f"{model_name}-stsb-simcse")
+except Exception:
+    logging.error(
+        f"Error uploading model to the Hugging Face Hub:\nTo upload it manually, you can run "
+        f"`huggingface-cli login`, followed by loading the model using `model = SentenceTransformer({latest_output_path!r})` "
+        f"and saving it using `model.push_to_hub('{model_name}-stsb-simcse')`."
+    )
