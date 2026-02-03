d2bdd6d18542e58ef603c95f7290ed89893bb52d
Omkar Kabde <omkarkabde@gmail.com>
2026-02-03 15:24:52 +0530

    migrate `train_askubuntu_ct.py` from v2 to v3 (#3647)
    * migrate `train_askubuntu_ct.py` from v2 to v3
    
    * Tiny comments, lower LR to (try to) prevent blowup
    
    ---------
    
    Co-authored-by: Tom Aarsen <Cubiegamedev@gmail.com>

--- examples/sentence_transformer/unsupervised_learning/CT/train_askubuntu_ct.py
+++ examples/sentence_transformer/unsupervised_learning/CT/train_askubuntu_ct.py
@@ -1,16 +1,22 @@
 import logging
+import random
+import traceback
 from datetime import datetime
 
-import torch
 from datasets import load_dataset
 
-from sentence_transformers import LoggingHandler, SentenceTransformer, evaluation, losses, models
+from sentence_transformers import LoggingHandler, SentenceTransformer
+from sentence_transformers.evaluation import RerankingEvaluator
+from sentence_transformers.losses import ContrastiveTensionLoss
+from sentence_transformers.models import Pooling, Transformer
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
 
 # Just some code to print debug information to stdout
 logging.basicConfig(
     format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO, handlers=[LoggingHandler()]
 )
-# /print debug information to stdout
+# print debug information to stdout
 
 # Some training parameters. We use a batch size of 16, for every positive example we include 8-1=7 negative examples
 # Sentences are truncated to 75 word pieces
@@ -39,43 +45,81 @@ for dataset in (eval_dataset, test_dataset):
 train_dataset = load_dataset("sentence-transformers/askubuntu-questions", split="train").filter(
     lambda x: x["text"] not in dev_or_test_questions
 )
+
+
+# Generate sentence pairs for ContrastiveTensionLoss
+def to_ct_pairs(sample, pos_neg_ratio=8):
+    pos_neg_ratio = 1 / pos_neg_ratio
+    return {
+        "text1": sample["text"],
+        "text2": sample["text"] if random.random() < pos_neg_ratio else random.choice(train_dataset)["text"],
+    }
+
+
+train_dataset = train_dataset.map(to_ct_pairs, fn_kwargs={"pos_neg_ratio": pos_neg_ratio}, remove_columns=["text"])
 logging.info(train_dataset)
+logging.info(train_dataset[0])
 
 # Initialize an SBERT model
-word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
-pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+word_embedding_model = Transformer(model_name, max_seq_length=max_seq_length)
+pooling_model = Pooling(word_embedding_model.get_word_embedding_dimension())
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-# Train the model
-
-# For ContrastiveTension we need a special data loader to construct batches with the desired properties
-train_dataloader = losses.ContrastiveTensionDataLoader(
-    train_dataset, batch_size=batch_size, pos_neg_ratio=pos_neg_ratio
-)
-
-# As loss, we losses.ContrastiveTensionLoss
-train_loss = losses.ContrastiveTensionLoss(model)
+# As loss, we use ContrastiveTensionLoss
+train_loss = ContrastiveTensionLoss(model)
 
 # Create a dev evaluator
-dev_evaluator = evaluation.RerankingEvaluator(eval_dataset, name="AskUbuntu dev")
-test_evaluator = evaluation.RerankingEvaluator(test_dataset, name="AskUbuntu test")
+dev_evaluator = RerankingEvaluator(eval_dataset, name="askubuntu-dev")
+test_evaluator = RerankingEvaluator(test_dataset, name="askubuntu-test")
 
+# Evaluate the model before training
+dev_evaluator(model)
+test_evaluator(model)
 
 logging.info("Start training")
-
-model.fit(
-    train_objectives=[(train_dataloader, train_loss)],
-    epochs=1,
-    weight_decay=0,
+# Prepare the training arguments
+args = SentenceTransformerTrainingArguments(
+    output_dir=output_path,
+    num_train_epochs=num_epochs,
+    per_device_train_batch_size=batch_size,
     warmup_steps=0,
-    optimizer_class=torch.optim.RMSprop,
-    optimizer_params={"lr": 1e-5},
-    use_amp=False,  # Set to True, if your GPU has optimized FP16 cores
+    learning_rate=2e-6,
+    weight_decay=0,
+    eval_strategy="steps",
+    eval_steps=1000,
+    save_strategy="steps",
+    save_steps=1000,
+    logging_steps=100,
+    fp16=False,  # Set to True, if your GPU has optimized FP16 cores
+    optim="rmsprop",
 )
 
-latest_output_path = output_path + "-latest"
-model.save(latest_output_path)
+# Train the model
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    loss=train_loss,
+    evaluator=dev_evaluator,
+)
+trainer.train()
 
-# Run test evaluation on the latest model. This is equivalent to not having a dev dataset
-model = SentenceTransformer(latest_output_path)
+logging.info("Evaluating after training")
+dev_evaluator(model)
 test_evaluator(model)
+
+# Save the trained & evaluated model locally
+final_output_dir = f"{output_path}/final"
+model.save_pretrained(final_output_dir)
+
+# (Optional) save the model to the Hugging Face Hub!
+# It is recommended to run `huggingface-cli login` to log into your Hugging Face account first
+model_name = model_name if "/" not in model_name else model_name.split("/")[-1]
+try:
+    model.push_to_hub(f"{model_name}-askubuntu-ct")
+except Exception:
+    logging.error(
+        f"Error uploading model to the Hugging Face Hub:\n{traceback.format_exc()}To upload it manually, you can run "
+        f"`huggingface-cli login`, followed by loading the model using `model = SentenceTransformer({final_output_dir!r})` "
+        f"and saving it using `model.push_to_hub('{model_name}-askubuntu-ct')`."
+    )
